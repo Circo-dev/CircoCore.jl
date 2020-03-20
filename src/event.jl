e5dd696ca3a817e952a7aa5196132f3256b4e756
Schaffer Krisztian <schaffer.krisztian@gmail.com>
2020-03-20 12:43:52 +0100

    Event dispatch basics added

---
+++ src/event.jl
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+
+abstract type Event end
+abstract type OneShotEvent <: Event end
+abstract type RecurringEvent <: Event end
+RecurrentEvent = RecurringEvent
+
+struct Subscribe{TEvent <: Event} # TODO: <: Request + handle forwarding
+    subscriber::Address
+end
+
+mutable struct EventDispatcher <: AbstractActor
+    listeners::Dict{Type{<:Event},Array{Address}}
+    address::Address
+    EventDispatcher() = new(Dict([]))
+end
+
+function onmessage(me::EventDispatcher, message::Subscribe{TEvent}, service) where {TEvent}
+    if !haskey(me.listeners, TEvent)
+        me.listeners[TEvent] = Array{Address}(undef, 0)
+    end
+    push!(me.listeners[TEvent], message.subscriber)
+end
+
+function onmessage(me::EventDispatcher, message::TEvent, service) where {TEvent <: Event}
+    for listener in get(me.listeners, TEvent, [])
+        send(service, me, listener, message)
+    end
+end
---
+++ test/event.jl
@@ -0,0 +1,63 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+using Test
+using CircoCore
+import CircoCore.onmessage, CircoCore.onschedule
+
+const TARGET_COUNT = 13
+const EVENT_COUNT = 133
+Start = Nothing
+
+struct TestEvent <: Event
+    value::String
+end
+
+mutable struct EventSource <: AbstractActor
+    eventdispatcher::Address
+    address::Address
+    EventSource() = new()
+end
+
+mutable struct EventTarget <: AbstractActor
+    received_count::UInt64
+    address::Address
+    EventTarget() = new(0)
+end
+
+# TODO: Create a Trait for that + handle multiple onschedule
+function onmessage(me::EventSource, message::Subscribe{TEvent}, service) where TEvent <: Event
+    send(service, me, me.eventdispatcher, message)
+end
+function fire(service, me::EventSource, event::TEvent) where TEvent <: Event
+    send(service, me, me.eventdispatcher, event)
+end
+function onschedule(me::EventSource, service)
+    me.eventdispatcher = spawn(service, EventDispatcher())
+    registername(service, "eventsource", me)
+end
+
+function onschedule(me::EventTarget, service)
+    send(service, me, getname(service, "eventsource"), Subscribe{TestEvent}(address(me)))
+end
+
+function onmessage(me::EventSource, message::Start, service)
+    for i=1:EVENT_COUNT
+        fire(service, me, TestEvent("Test event #$i"))
+    end
+end
+
+function onmessage(me::EventTarget, message::TestEvent, service)
+    me.received_count += 1
+end
+
+@testset "Actor" begin
+    @testset "Actor-Tree" begin
+        source = EventSource()
+        targets = [EventTarget() for i=1:TARGET_COUNT]
+        scheduler = ActorScheduler([source; targets])
+        @time scheduler(Message{Start}(address(source)))
+        for target in targets
+            @test target.received_count == EVENT_COUNT
+        end
+        shutdown!(scheduler)
+    end
+end
