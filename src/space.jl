1b15b16d6a983b9a1c83ba271ecd45b3c8b55349
Schaffer Krisztian <schaffer.krisztian@gmail.com>
2020-04-30 21:53:00 +0200

    Arbitrary names for magic numbers

--- src/space.jl
+++ src/space.jl
@@ -7,12 +7,15 @@ end
 
 infotonhandler(plugin::SpaceService) = apply_infoton
 
+const I = 1.0
+const TARGET_DISTANCE = 150
+
 function apply_infoton(space::SpaceService, scheduler, targetactor::AbstractActor, message)
     diff = message.infoton.sourcepos - targetactor.core.pos
     difflen = norm(diff)
-    if difflen < 150.0
+    if difflen < TARGET_DISTANCE
         return nothing
     end
-    targetactor.core.pos += diff / difflen
+    targetactor.core.pos += diff / (difflen * I)
     return nothing
 end
