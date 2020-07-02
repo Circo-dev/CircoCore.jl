5f88ce7afa80e01e41c0ceb17bf319b2c1cf3a1d
Schaffer Krisztian <schaffer.krisztian@gmail.com>
2020-07-02 14:23:40 +0200

    Reverted cluster.jl

--- src/cluster/cluster.jl
+++ src/cluster/cluster.jl
@@ -106,7 +106,6 @@ struct ForceAddRoot
 end
 
 function requestjoin(me::ClusterActor, service)
-    @debug "$(addr(me)) : Requesting join"
     if !isempty(me.servicename)
         registername(service, NAME, me)
     end
@@ -117,19 +116,22 @@ function requestjoin(me::ClusterActor, service)
     if me.joinrequestcount >= MAX_JOINREQUEST_COUNT
         error("Cannot join: $(me.joinrequestcount) unsuccesful attempt.")
     end
-    querycluster_thensendjoinrequest(me, rand(me.roots), service)
+    sendjoinrequest(me, rand(me.roots), service)
 end
 
-function querycluster_thensendjoinrequest(me::ClusterActor, root::PostCode, service)
+function sendjoinrequest(me::ClusterActor, root::PostCode, service)
     me.joinrequestcount += 1
-    @debug "$(addr(me)) : Querying name 'cluster'"
-    send(service, me, Addr(root), NameQuery("cluster");timeout=Second(10))
+    rootaddr = Addr(root)
+    if isbaseaddress(rootaddr)
+        send(service, me, Addr(root), NameQuery("cluster");timeout=Second(10))
+    else
+        send(service, me, rootaddr, JoinRequest(me.myinfo))
+    end
 end
-
-function onmessage(me::ClusterActor, msg::ForceAddRoot, service)
-    @debug "$(addr(me)) : Got $msg"
-    push!(me.roots, msg.root)
-    querycluster_thensendjoinrequest(me, msg.root, service)
+ 
+function onmessage(me::ClusterActor, messsage::ForceAddRoot, service)
+    push!(me.roots, messsage.root)
+    sendjoinrequest(me, messsage.root, service)
 end
 
 function onschedule(me::ClusterActor, service)
@@ -145,17 +147,13 @@ function setpeer(me::ClusterActor, peer::NodeInfo)
         return false
     end
     me.peers[peer.addr] = peer
-    @debug "$(addr(me)) : Peer $(addr(peer)) set"
     return true
 end
 
 function registerpeer(me::ClusterActor, newpeer::NodeInfo, service)
     if setpeer(me, newpeer)
-        event = PeerListUpdated(collect(values(me.peers)))
-        @debug "$(addr(me)) : Firing $event"
-        fire(service, me, event)
+        fire(service, me, PeerListUpdated(collect(values(me.peers))))
         for friend in me.downstream_friends
-            @debug "$(addr(me)) : Sending out PeerJoinedNotification to $friend"
             send(service, me, friend, PeerJoinedNotification(newpeer, addr(me)))
         end
         return true
@@ -163,45 +161,31 @@ function registerpeer(me::ClusterActor, newpeer::NodeInfo, service)
     return false
 end
 
-function onmessage(me::ClusterActor, msg::Subscribe{Joined}, service)
+function onmessage(me::ClusterActor, messsage::Subscribe{Joined}, service)
     if me.joined
-        event = Joined(collect(values(me.peers)))
-        @debug "$(addr(me)) : Sending out event to late subscriber $(msg.subscriber): $event"
-        send(service, me, msg.subscriber, event) #TODO handle late subscription to one-off events automatically
+        send(service, me, messsage.subscriber, Joined(collect(values(me.peers)))) #TODO handle late subscription to one-off events automatically
     end
-    @debug "$(addr(me)) : New subscriber to Joined $(msg.subscriber)"
-    send(service, me, me.eventdispatcher, msg)
+    send(service, me, me.eventdispatcher, messsage)
 end
 
-function onmessage(me::ClusterActor, msg::Subscribe{PeerListUpdated}, service)
+function onmessage(me::ClusterActor, messsage::Subscribe{PeerListUpdated}, service)
     if length(me.peers) > 0
-        event = PeerListUpdated(collect(values(me.peers)))
-        @debug "$(addr(me)) : Sending out event to late subscriber $(msg.subscriber): $event"
-        send(service, me, msg.subscriber, event) # TODO State-change events may need a better (automatic) mechanism for handling initial state
+        send(service, me, messsage.subscriber, PeerListUpdated(collect(values(me.peers)))) # TODO State-change events may need a better (automatic) mechanism for handling initial state
     end
-    send(service, me, me.eventdispatcher, msg)
+    send(service, me, me.eventdispatcher, messsage)
 end
 
-function onmessage(me::ClusterActor, msg::NameResponse, service)
-    if msg.query.name !== "cluster"
-        @info "$(addr(me)) : Got invalid $msg"
-        return
-    end
-    root = msg.handler
+function onmessage(me::ClusterActor, message::NameResponse, service)
+    root = message.handler
     if isnothing(root)
         requestjoin(me, service)
     else
-        sendjoinrequest(me, root, service)
+        send(service, me, root, JoinRequest(me.myinfo))
     end
 end
 
-function sendjoinrequest(me::ClusterActor, root::Addr, service)
-    send(service, me, root, JoinRequest(me.myinfo))
-end
-
-function onmessage(me::ClusterActor, msg::JoinRequest, service)
-    @debug "$(addr(me)) : Got $msg"
-    newpeer = msg.info
+function onmessage(me::ClusterActor, message::JoinRequest, service)
+    newpeer = message.info
     if (length(me.upstream_friends) < TARGET_FRIEND_COUNT)
         send(service, me, newpeer.addr, FriendRequest(addr(me)))
     end
@@ -211,13 +195,12 @@ function onmessage(me::ClusterActor, msg::JoinRequest, service)
     send(service, me, newpeer.addr, JoinResponse(newpeer, me.myinfo, collect(values(me.peers)), true))
 end
 
-function onmessage(me::ClusterActor, msg::JoinResponse, service)
-    @debug "$(addr(me)) : Got $msg"
-    if msg.accepted
+function onmessage(me::ClusterActor, message::JoinResponse, service)
+    if message.accepted
         me.joined = true
-        initpeers(me, msg.peers, service)
+        initpeers(me, message.peers, service)
         send(service, me, me.eventdispatcher, Joined(collect(values(me.peers))))
-        @info "Joined to cluster using root node $(msg.responderinfo.addr). ($(length(msg.peers)) peers)"
+        @info "Joined to cluster using root node $(message.responderinfo.addr). ($(length(message.peers)) peers)"
     else
         requestjoin(me, service)
     end
@@ -233,15 +216,13 @@ function initpeers(me::ClusterActor, peers::Array{NodeInfo}, service)
     end
 end
 
-function onmessage(me::ClusterActor, msg::PeerListRequest, service)
-    @debug "$(addr(me)) : Got $msg"
-    send(service, me, msg.respondto, PeerListResponse(collect(values(me.peers))))
+function onmessage(me::ClusterActor, message::PeerListRequest, service)
+    send(service, me, message.respondto, PeerListResponse(collect(values(me.peers))))
 end
 
-function onmessage(me::ClusterActor, msg::PeerJoinedNotification, service)
-    @debug "$(addr(me)) : Got $msg"
-    if registerpeer(me, msg.peer, service)
-        friend = get(me.upstream_friends, msg.creditto, nothing)
+function onmessage(me::ClusterActor, message::PeerJoinedNotification, service)
+    if registerpeer(me, message.peer, service)
+        friend = get(me.upstream_friends, message.creditto, nothing)
         isnothing(friend) && return
         friend.score += 1
         if friend.score == 100
@@ -259,14 +240,11 @@ function onmessage(me::ClusterActor, msg::PeerJoinedNotification, service)
 end
 
 function getanewfriend(me::ClusterActor, service)
-    @debug "$(addr(me)) : getanewfriend with peer count: $(length(me.peers))"
     length(me.peers) > 0 || return
     while true
         peer = rand(me.peers)[2]
         if peer.addr != addr(me)
-            req = FriendRequest(addr(me))
-            @debug "$(addr(me)) : Sending $req"
-            send(service, me, peer.addr, req)
+            send(service, me, peer.addr, FriendRequest(addr(me)))
             return nothing
         end
     end
@@ -290,14 +268,13 @@ function replaceafriend(me::ClusterActor, service)
     end
 end
 
-function onmessage(me::ClusterActor, msg::FriendRequest, service)
-    @debug "$(addr(me)) : Got $msg"
-    friendsalready = msg.requestor in me.downstream_friends
+function onmessage(me::ClusterActor, message::FriendRequest, service)
+    friendsalready = message.requestor in me.downstream_friends
     accepted = !friendsalready && length(me.downstream_friends) < MAX_DOWNSTREAM_FRIENDS
     if accepted
-        push!(me.downstream_friends, msg.requestor)
+        push!(me.downstream_friends, message.requestor)
     end
-    send(service, me, msg.requestor, FriendResponse(addr(me), accepted))
+    send(service, me, message.requestor, FriendResponse(addr(me), accepted))
 end
 
 function onmessage(me::ClusterActor, message::FriendResponse, service)
