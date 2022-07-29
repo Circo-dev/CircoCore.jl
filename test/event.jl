55da4413be95b1b5f6af19de98c778cc3b1256f5
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2022-07-29 12:35:51 +0200

    Event filtering by topic or predicate + unsubscibe (#34)

--- test/event.jl
+++ test/event.jl
@@ -7,7 +7,12 @@ const TARGET_COUNT = 13
 const EVENT_COUNT = 133
 struct Start end
 
-struct TestEvent <: Event
+struct NonTopicEvent <: Event
+    value::String
+end
+
+struct TopicEvent <: Event
+    topic::String
     value::String
 end
 
@@ -18,10 +23,11 @@ end
 EventSource(core) = EventSource(Addr(), core)
 
 mutable struct EventTarget{TCore} <: Actor{TCore}
-    received_count::Int64
+    received_nontopic_count::Int64
+    received_topic_count::Int64
     core::TCore
 end
-EventTarget(core) = EventTarget(0, core)
+EventTarget(core) = EventTarget(0, 0, core)
 
 function onspawn(me::EventSource, service)
     me.eventdispatcher = spawn(service, EventDispatcher(emptycore(service)))
@@ -29,17 +35,26 @@ function onspawn(me::EventSource, service)
 end
 
 function onspawn(me::EventTarget, service)
-    send(service, me, getname(service, "eventsource"), Subscribe{TestEvent}(addr(me)))
+    eventsource = getname(service, "eventsource")
+    send(service, me, eventsource, Subscribe(NonTopicEvent, addr(me)))
+    send(service, me, eventsource, Subscribe(TopicEvent, addr(me), "topic3"))
+    send(service, me, eventsource, Subscribe(TopicEvent, addr(me), "topic4"))
+    send(service, me, eventsource, Subscribe(TopicEvent, addr(me), event -> event.topic == "topic5"))
 end
 
 function onmessage(me::EventSource, message::Start, service)
     for i=1:EVENT_COUNT
-        fire(service, me, TestEvent("Test event #$i"))
+        fire(service, me, NonTopicEvent("Test event #$i"))
+        fire(service, me, TopicEvent("topic$i", "Topic event #$i"))
     end
 end
 
-function onmessage(me::EventTarget, message::TestEvent, service)
-    me.received_count += 1
+function onmessage(me::EventTarget, message::NonTopicEvent, service)
+    me.received_nontopic_count += 1
+end
+
+function onmessage(me::EventTarget, message::TopicEvent, service)
+    me.received_topic_count += 1
 end
 
 @testset "Actor" begin
@@ -50,10 +65,21 @@ end
         scheduler = Scheduler(ctx, [source; targets])
         scheduler(;remote = false) # to spawn the zygote
         send(scheduler, source, Start())
-        @time scheduler(;remote = false)
+        scheduler(;remote = false)
         for target in targets
-            @test target.received_count == EVENT_COUNT
+            @test target.received_nontopic_count == EVENT_COUNT
+            @test target.received_topic_count == 3
         end
+
+        # unsubscribe and rerun
+        send(scheduler, source, UnSubscribe(addr(source), TopicEvent))
+        send(scheduler, source, Start())
+        scheduler(;remote = false)
+        for target in targets
+            @test target.received_nontopic_count == 2 * EVENT_COUNT
+            @test target.received_topic_count == 3
+        end
+
         shutdown!(scheduler)
     end
 end
