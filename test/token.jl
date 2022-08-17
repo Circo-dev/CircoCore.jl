84cd968ec4d019435ca3477721ddb3f45eee4ccb
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2022-08-17 11:56:17 +0200

    Reply and Failure

--- test/token.jl
+++ test/token.jl
@@ -4,9 +4,8 @@ module TokenTest
 using Test
 using Dates
 using CircoCore
-import CircoCore: onmessage
 
-MESSAGE_COUNT = 100
+MESSAGE_COUNT = 201
 
 struct TRequest <: Request
     id::UInt64
@@ -15,14 +14,20 @@ struct TRequest <: Request
 end
 
 mutable struct Requestor{TCore} <: Actor{TCore}
-    responsecount::Int
+    replycount::Int
+    failurecount::Int
     timeoutcount::Int
     responder::Addr
     core::TCore
+    Requestor(core) = new{typeof(core)}(0, 0, 0, Addr(), core)
 end
-Requestor(core) = Requestor(0, 0, Addr(), core)
 
-struct TResponse <: Response
+struct TReply <: Reply
+    requestid::UInt64
+    token::Token
+end
+
+struct TFailure <: Failure
     requestid::UInt64
     token::Token
 end
@@ -31,34 +36,41 @@ mutable struct Responder{TCore} <: Actor{TCore}
     core::TCore
 end
 
-function onmessage(me::Responder, ::OnSpawn, service)
+CircoCore.onmessage(me::Responder, ::OnSpawn, service) = begin
     registername(service, string(TRequest), me)
 end
 
-function onmessage(me::Requestor, ::OnSpawn, service)
-    registername(service, string(TResponse), me)
+CircoCore.onmessage(me::Requestor, ::OnSpawn, service) = begin
+    registername(service, "requestor", me)
     me.responder = getname(service, string(TRequest))
     for i=1:MESSAGE_COUNT
         send(service, me, me.responder, TRequest(i); timeout = 2.0)
     end
 end
 
-function onmessage(me::Responder, req::TRequest, service)
-    if req.id % 2 == 1
-        send(service, me, getname(service, string(TResponse)), TResponse(req.id, req.token))
+CircoCore.onmessage(me::Responder, req::TRequest, service) = begin
+    if req.id % 3 == 1
+        send(service, me, getname(service, "requestor"), TReply(req.id, req.token))
+    end
+    if req.id % 3 == 2
+        send(service, me, getname(service, "requestor"), TFailure(req.id, req.token))
     end
     if req.id == MESSAGE_COUNT
         die(service, me)
     end
 end
 
-function onmessage(me::Requestor, resp::TResponse, service)
-    me.responsecount += 1
+CircoCore.onmessage(me::Requestor, resp::TReply, service) = begin
+    me.replycount += 1
 end
 
-function onmessage(me::Requestor, timeout::Timeout, service)
+CircoCore.onmessage(me::Requestor, resp::TFailure, service) = begin
+    me.failurecount += 1
+end
+
+CircoCore.onmessage(me::Requestor, timeout::Timeout, service) = begin
     me.timeoutcount += 1
-    if me.timeoutcount == MESSAGE_COUNT / 2
+    if me.timeoutcount == MESSAGE_COUNT / 3
         println("Got $(me.timeoutcount) timeouts, exiting.")
         die(service, me; exit=true)
     end
@@ -71,7 +83,8 @@ end
     scheduler = Scheduler(ctx, [responder, requestor])
     scheduler(;remote=true)
     @test requestor.responder == addr(responder)
-    @test requestor.responsecount == MESSAGE_COUNT / 2
+    @test requestor.replycount == MESSAGE_COUNT / 3
+    @test requestor.failurecount == MESSAGE_COUNT / 3
     @test length(scheduler.tokenservice.timeouts) == 0
     shutdown!(scheduler)
 end
